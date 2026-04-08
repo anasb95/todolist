b40e4ee6d362ee6e9e2e169667ada450bf7cfdf0
anasb95 <nounousb93@gmail.com>
2026-04-08 12:23:52 +0200

    Ajout du formulaire

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ function App() {
     <div>
       <h1>Ma Todolist</h1>
       <p>Vue principale - version 1</p>
+      <input type="text" placeholder="Ajouter une tâche..." />
+      <button>Ajouter</button>
     </div>
   );
 }
